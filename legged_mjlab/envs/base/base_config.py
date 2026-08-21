a5d54417d7744c288ac544acc855e5fa6c232aa8
奈奈奈奈奈绪 <2171670754@qq.com>
2026-08-21 19:12:00 +0800

    Create base_config.py

---
+++ legged_mjlab/envs/base/base_config.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+import inspect
+from copy import deepcopy
+from functools import partial
+from pathlib import Path
+
+
+class BaseConfig:
+    def __init__(self) -> None:
+        self.init_member_classes(self)
+
+    @staticmethod
+    def init_member_classes(obj: object) -> None:
+        for key in dir(obj):
+            if key == "__class__":
+                continue
+            value = getattr(obj, key)
+            if inspect.isclass(value):
+                instance = value()
+                setattr(obj, key, instance)
+                BaseConfig.init_member_classes(instance)
+            elif isinstance(value, (dict, list, set)):
+                setattr(obj, key, deepcopy(value))
