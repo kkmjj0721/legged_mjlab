97ac4bd269b68a0752b18cadfa288954b425dfdb
奈奈奈奈奈绪 <2171670754@qq.com>
2026-08-24 19:20:10 +0800

    Update him_go2_env.py

--- legged_mjlab/envs/him_go2/him_go2_env.py
+++ legged_mjlab/envs/him_go2/him_go2_env.py
@@ -10,7 +10,13 @@ from legged_mjlab.envs.him_go2.him_go2_config import HimGo2RoughCfg
 
 
 class HimGo2Env(ManagerBasedRlEnv):
-    def __init__(self, cfg: HimGo2RoughCfg, sim_device):
+    def __init__(self, cfg: HimGo2RoughCfg, sim_device, render_mode):
+        """ 
+        Args:
+            cfg (HimGo2RoughCfg): 配置对象，包含环境、控制器和资产的参数
+            sim_device: 模拟设备（如 CPU 或 GPU）
+            render_mode: 渲染模式（如 "human" 或 "rgb_array"）
+        """
         self.cfg = cfg
 
         self.managercfg = self._build_mjlab_managercfg(self.cfg)
@@ -26,4 +32,22 @@ class HimGo2Env(ManagerBasedRlEnv):
         """ 将 HimGo2RoughCfg 转换为 ManagerBasedRlEnvCfg 以便于使用 mjlab 的管理器进行环境管理。
         """
         asset = Go2Asset(cfg)
+
+
+    def _build_scene(cls, cfg, asset):
+        """ 构建场景对象，包含地形和机器人实体。
+        """
+
+    def _build_actions(self):
+        """
+        """
+
+    def _build_commands(self):
+        """
+        """
+
+    def _build_rewards(self):
+        """
+        """
+
         
